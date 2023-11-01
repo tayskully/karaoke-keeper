8ae9e31b4588052c89f3c4caab80d53dc8919aa1
Fmquito45 <fmquitox45@gmail.com>
2023-11-01 11:50:46 -0400

    adds function Signup

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -4,3 +4,9 @@ import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
 import { react } from 'react'
 import Auth from '../utils/auth';
+
+function Signup () {
+
+}
+
+export default Signup;
