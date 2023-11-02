dc5493ca0e1b06876a35f2a7bb05b9ed6fc9091d
fatimaminhas <f.minhas394@gmail.com>
2023-11-02 10:49:49 -0400

    added profile page and endpoint for it

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -25,10 +25,10 @@ const router = createBrowserRouter([
         path: "/signup",
         element: <Signup />,
       },
-      // {
-      //   path: "/me",
-      //   element: <Profile />,
-      // },
+      {
+        path: "/me",
+        element: <Profile />,
+      },
     ],
   },
 ]);
