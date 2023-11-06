641ad013240ed73efe2c55dd566698335ca971a7
wayne80361 <wayne80361@gmail.com>
2023-11-06 00:08:09 -0500

    hide add song to profile button when not logged in

--- client/src/components/SongCard.jsx
+++ client/src/components/SongCard.jsx
@@ -6,17 +6,19 @@ import { useLazyQuery, useMutation } from "@apollo/client";
 import { ADD_SONG } from "../utils/mutation";
 import { GET_SINGLE_SONG } from "../utils/queries";
 
+import Auth from "../utils/auth";
+import { saveSongIds, getSavedSongIds } from "../utils/localStorage";
+
 export default function SongCard({ song }) {
   const [getSong] = useLazyQuery(GET_SINGLE_SONG);
   const [addSong] = useMutation(ADD_SONG);
 
   const handleAddSong = async () => {
     try {
-
       // get lyrics
-      const songResult = await getSong({ variables: { songId: song.songId } })
+      const songResult = await getSong({ variables: { songId: song.songId } });
 
-      const lyrics = songResult.data.song.lyrics
+      const lyrics = songResult.data.song.lyrics;
 
       const { data } = await addSong({
         variables: {
@@ -24,15 +26,13 @@ export default function SongCard({ song }) {
           title: song.title,
           artist: song.artist,
           image: song.image,
-          lyrics
+          lyrics,
         },
       });
-
-
     } catch (e) {
       console.error(e);
     }
-  }
+  };
 
   return (
     <div className="d-flex flex-row">
@@ -48,9 +48,7 @@ export default function SongCard({ song }) {
                 {/* <a href="/songs/{song.id}">View Song</a> */}
                 <Link to={`/songs/${song.songId}`}> view song</Link>
               </Button>
-              <Button onClick={handleAddSong}>
-                add to profile
-              </Button>
+              {Auth.loggedIn() && <Button>add to profile</Button>}
             </Card.Description>
           </Card.Content>
         </Card>
